affc16e540582290b03ec3af4586df12637f2ba7
Luigi Petrucco <luigi.petrucco@gmail.com>
2022-01-22 22:49:52 +0100

    requirements dev file

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import find_namespace_packages, setup
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("requirements_dev.txt") as f:
+    requirements_dev = f.read().splitlines()
+
 with open("README.md") as f:
     long_description = f.read()
 
@@ -13,8 +16,9 @@ setup(
     install_requires=requirements,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires=">=3.6",
+    python_requires="==3.8",
     packages=find_namespace_packages(exclude=("docs", "tests*")),
+    extras_require=dict(dev=requirements_dev),
     include_package_data=True,
     url="https://github.com/portugueslab/xiao_et_al",
     author="Luigi Petrucco",
